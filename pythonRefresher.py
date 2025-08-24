fbdab0036d60da12d207bbcc2170abd224022752
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-24 20:43:53 +0600

    If Else Statement

--- pythonRefresher.py
+++ pythonRefresher.py
@@ -1,5 +1,13 @@
-age = 30
+age = 24
 
 name = "Jubaer"
 
+todayIsCold = True
+
 print(f"Hello, my name is {name} and I am {age} years old.")
+
+if age > 18 :
+    print("You are an adult.")
+
+else :
+    print("You are not an adult.")
